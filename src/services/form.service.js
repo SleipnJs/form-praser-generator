b378df4f2e0448bc7d9c5c77d5099396f638c2ed
Dominik Pająk <dominik00801@gmail.com>
2021-03-28 00:42:52 +0100

    form-service

--- src/services/form.service.js
+++ src/services/form.service.js
@@ -4,6 +4,9 @@ class FormDataService {
   getForm(id, stage) {
     return http.get("/data/forms?id="+id+"&stage="+stage);
   }
+  getMostLiked(amount) {
+    return http.get("formsMostLiked?amount="+amount);
+  }
   create(data) {
     return http.post("/data/forms", data);
   }
