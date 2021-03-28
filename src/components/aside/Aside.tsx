bc7841ff652391f39a48cb2c93e97d93414bd855
Isaayy <kuba.serwin99@gmail.com>
2021-03-28 09:03:36 +0200

    Form generator fix

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -5,7 +5,7 @@ import './Aside.scss';
 const Aside = () => {
   return (
     <aside className="aside">
-      <h2 className="subheading">I ty możesz stworzyć swoją ankietę</h2>
+      <h2 className="subheading">I Ty możesz stworzyć swoją ankietę</h2>
       <p className="desc">Stwórz konto i dołącz do naszego grona użytkowników</p>
       <button className="aside__btn aside__btn--red">Utwórz konto</button>
     </aside>
